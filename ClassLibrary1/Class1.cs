47d892cbaac2f7f7fa671f35ddfe5d7c60ef294b
Anitha <pasupuax@legal.regn.net>
2018-12-06 03:39:57 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿//using OpenQA.Selenium;
+
+//using OpenQA.Selenium.Chrome;
+//using TechTalk.SpecFlow;
+
+//namespace ClassLibrary1
+//{
+//    [Binding]
+//    public class Class1
+//    {
+//        private IWebDriver driver;
+        
+//        [Given(@"I have entered (.*) into the calculator")]
+//        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+//        {
+//            //driver = new ChromeDriver();
+//            //Class2 c = new Class2(driver);
+//            //c.goToPage();
+//            //c.cliclbutton();
+//        }
+
+//        [When(@"I press add")]
+//        public void WhenIPressAdd()
+//        {
+//            ScenarioContext.Current.Pending();
+//        }
+
+//        [Then(@"the result should be (.*) on the screen")]
+//        public void ThenTheResultShouldBeOnTheScreen(int p0)
+//        {
+//            ScenarioContext.Current.Pending();
+//        }
+
+//    }
+//}
---
+++ ClassLibrary1/Class2.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Support;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Support.PageObjects;
+
+//namespace ClassLibrary1
+//{
+//   public class Class2
+//    {
+        
+       
+//        private IWebDriver driver;
+//        public Class2(IWebDriver driver)
+//        {
+//            this.driver = driver;
+//            PageFactory.InitElements(driver, this);
+//        }
+
+//        [FindsBy(How = How.XPath, Using = "//*[@id='pod - content - z8rL9gk']/ul/li/ul[1]/li[2]/button")]
+//        private IWebElement about;
+
+//        public void cliclbutton()
+//        {
+//            about.Click();
+//        }
+//        public void goToPage()
+//        {
+//            driver.Navigate().GoToUrl("https://advance.lexis.com");
+//        }
+//    }
+//}
